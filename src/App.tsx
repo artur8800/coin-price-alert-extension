53714bc0649014972dafdddf6c00ce879acf4908
Artur <artursviridonov@gmail.com>
2026-02-08 22:38:50 +0200

    feat: empty state was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import AppEmptyTasks from "@/components/empty-state/empty-tasks";
 
 function App() {
-  // start bilding your popup app here
   return (
-    <div className="bg-white w-screen-400 min-h-screen-min max-h-screen-max p-4">
-      <h1 className="text-3xl text-center font-bold underline">
-        <Button>test</Button>
-      </h1>
+    <div className="bg-white w-screen-400 min-h-screen-min max-h-screen-max p-4 relative">
+      <AppEmptyTasks />
     </div>
   );
 }
