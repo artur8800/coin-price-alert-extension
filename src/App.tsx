8158a8c10cb577bf986afa575b8aac50d7b7da9d
Artur <artursviridonov@gmail.com>
2026-02-08 21:40:16 +0200

    feat: min screen height and width was added to tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { Button } from "./components/ui/button";
 function App() {
   // start bilding your popup app here
   return (
-    <h1 className="text-3xl text-center font-bold underline">
-      <Button>test</Button>
-    </h1>
+    <div className="bg-white w-screen-400 min-h-screen-min max-h-screen-max p-4">
+      <h1 className="text-3xl text-center font-bold underline">
+        <Button>test</Button>
+      </h1>
+    </div>
   );
 }
 
